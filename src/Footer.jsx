a1d1898bd87b3783ac74158113d27c1f4bc7681c
Felix Pherry <123felixpherry@gmail.com>
2022-08-13 14:50:00 +0800

    login page optimization

---
+++ src/Footer.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Footer.css';
+
+function Footer() {
+  return (
+    <div className="footer">
+        <img src="https://www.pngkey.com/png/full/32-324267_design-manager-linkedin-linkedin.png" alt="" />
+        <p>&#169; 2022</p>
+        <p>User Agreement</p>
+        <p>Privacy Policy</p>
+        <p>Community Guidelines</p>
+        <p>Cookie Policy</p>
+        <p>Copyright Policy</p>
+        <p>Send Feedback</p>
+    </div>
+  )
+}
+
+export default Footer;
--- src/Login.jsx
+++ src/Login.jsx
@@ -4,12 +4,14 @@ import { auth } from './firebase';
 import './Login.css';
 import { useDispatch } from 'react-redux';
 import { login } from './features/userSlice';
+import Footer from './Footer';
 
 function Login() {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [name, setName] = useState('');
     const [profilePic, setProfilePic] = useState('');
+	const [signup, setSignup] = useState(false);
     const dispatch = useDispatch();
 
     const loginToApp = (e) => {
@@ -19,12 +21,11 @@ function Login() {
 		});
 	};
 
-	const register = () => {
+	const register = (e) => {
+		e.preventDefault();
 		if (!name) {
 			return alert('Please enter a full name');
 		}
-
-		console.log('register the user');
 
 		createUserWithEmailAndPassword(auth, email, password)
 			.then((userAuth) => {
@@ -42,38 +43,64 @@ function Login() {
 							})
 						)
 					)
-					.catch((error) => {
-						console.log('user not updated');
+					.catch((e) => {
+						console.log(e);
 					});
 			})
-			.catch((err) => {
-				alert(err);
+			.catch((e) => {
+				alert(e);
 			});
 	};
 
-
-  return (
-    <>
-        <div className="login_logo">
-            <img src="https://www.pngkey.com/png/full/32-324267_design-manager-linkedin-linkedin.png" alt="" />
-        </div>
-        <div className="login">
-            <div className="login_header">
-                <h1>Sign in</h1>
-                <p>Stay updated on your professional world</p>
-            </div>
-           <form>
-                <input value={name} onChange={(e) => setName(e.target.value)} placeholder="Full Name (Required)" type="text" />
-                <input value={profilePic} onChange={(e) => setProfilePic(e.target.value)} placeholder="Profile Pic URL (Optional)" type="text" />
-                <input value={email} onChange={(e) => setEmail(e.target.value)} placeholder="Email" type="email" />
-                <input value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Password" type="password" />
-                <button onClick={loginToApp} type="submit">Sign In</button>
-           </form>
-        </div>
-        <p className="login_register">New to Linkedin?{"   "}
-            <span className="login_register_span" onClick={register}>Join now</span>
-        </p>
-    </>
+  return (<>
+	{!signup ? (
+		<>
+			<div className="login_logo">
+				<img src="https://www.pngkey.com/png/full/32-324267_design-manager-linkedin-linkedin.png" alt="" />
+			</div>
+			<div className="login">
+				<div className="login_header">
+					<h1>Sign in</h1>
+					<p>Stay updated on your professional world</p>
+				</div>
+				<form>
+						<input value={email} onChange={(e) => setEmail(e.target.value)} placeholder="Email" type="email" />
+						<input value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Password" type="password" />
+						<label htmlFor="forgotPassword">Forgot password?</label>
+						<button onClick={loginToApp} type="submit">Sign In</button>
+				</form>
+			</div>
+			<p className="login_register">New to LinkedIn?{"   "}
+				<span className="login_register_span" onClick={() => setSignup(true)}>Join now</span>
+			</p>
+			<Footer />
+		</>
+	) : (
+		<>
+			<div className="register_logo">
+				<img src="https://www.pngkey.com/png/full/32-324267_design-manager-linkedin-linkedin.png" alt="" />
+			</div>
+			<h1 className="register_heading">Make the most of your professional life</h1>
+			<div className="register">
+				<form>
+						<label htmlFor="full-name">Full Name (Required)</label>
+						<input value={name} onChange={(e) => setName(e.target.value)} type="text" />
+						<label htmlFor="profile-pic">Profile Picture URL (Optional)</label>
+						<input value={profilePic} onChange={(e) => setProfilePic(e.target.value)} type="text" />
+						<label htmlFor="email">Email</label>
+						<input value={email} onChange={(e) => setEmail(e.target.value)} type="email" />
+						<label htmlFor="password">Password</label>
+						<input value={password} onChange={(e) => setPassword(e.target.value)} type="password" />
+						<button onClick={register}>Agree & Join</button>
+				</form>
+				<p className="new_register">Already on LinkedIn?{"   "}
+					<span className="new_register_span" onClick={() => {setSignup(false)}}>Sign in</span>
+				</p>
+			</div>
+			<Footer />
+		</>
+	)}
+	</>
   )
 }
 
